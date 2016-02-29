dd1b5d852da29c60a1925cea7a50e705087d6432
Mark Brown <markbr.wn@gmail.com>
2016-02-29 14:55:37 -0800

    first commit

---
+++ mail.py
@@ -0,0 +1,42 @@
+from smtplib import SMTP_SSL as SMTP
+import logging
+import logging.handlers
+import sys
+from email.mime.text import MIMEText
+
+
+def send_confirmation():
+    text= """This is a small python module to send mail. This text will be the body of the message.""" 
+    msg = MIMEText(text, 'plain')
+    msg['Subject'] = "The Subject" 
+    me ='markbr.wn@gmail.com'
+    msg['To'] = 'mark@oeno.io'
+    try:
+        conn = SMTP('smtp.gmail.com')
+        conn.set_debuglevel(True)
+        conn.login('login (no @domain.com)', 'password')
+        try:
+            conn.sendmail(me, me, msg.as_string())
+        finally:
+            conn.close()
+#?
+    except Exception as exc:
+        logger.error("ERROR!!!")
+        logger.critical(exc)
+        sys.exit("Mail failed: {}".format(exc))
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    random_condition = True
+
+    if random_condition:
+        send_confirmation()
